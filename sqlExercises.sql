fbc4c8c7398254500537c4b82a619e0620870989
Jacob Luber <119356072+JacobLuber2@users.noreply.github.com>
2023-04-24 07:04:04 -0800

    Add files via upload

--- sqlExercises.sql
+++ sqlExercises.sql
@@ -1,36 +1,52 @@
--- find all products
-select * from bestbuy.products;
--- find all products that cost $1400
-SELECT * FROM  bestbuy.products
-WHERE products.Price = 1400.00;
 
--- find all products that cost $11.99 or $13.99
-Select * from products where price = 11.99 or price = 13.99;
--- find all products that do NOT cost 11.99 - using NOT
-select * from products where not price = 11.99;
+select p.name as product, c.name  as category
+from products as p
+inner join categories as c
+on p.categoryID = c.CategoryID
+where c.name = "computers";
 
--- find all products and sort them by price from greatest to least
-select * from products order by price asc;
-
-
--- find all employees who don't have a middle initial
-select * from employees where middleinitial is null;
--- find distinct product prices
-select distinct products.price from products;
--- find all employees whose first name starts with the letter ‘j’
-select * from employees where firstname like 'j%';
-
--- find all Macbooks
-select * from products where name = 'macbook';
+select p.name, p.price, r.rating
+from products as p
+inner join reviews as r
+on p.productID = r.productID
+where rating = 5;
+/* joins: find the employee with the most total quantity sold.  use the sum() function and group by */
+select concat(e.firstname, " ", e.lastname) as employee, sum(s.quantity) as total
+from employees as e
+inner join sales as s
+on e.employeeID = s.employeeID
+group by e.employeeID
+order by total desc
+limit 599;
 
 
--- find all products that are on sale
-select * from products where OnSale = true;
+/* joins: find the name of the department, and the name of the category for Appliances and Games */
+select d.name as department, c.name as category from categories as c
+inner join departments as d
+on d.DepartmentID = c.departmentID
+where c.name = "Appliances" or c.name = "Games";
 
--- find the average price of all products
-SELECT AVG(products.price) FROM bestbuy.products;
 
--- find all Geek Squad employees who don't have a middle initial
-select * from employees where title = 'Geek Squad' and MiddleInitial is null;
--- find all products from the products table whose stock level is in the range 
-select * from products where StockLevel between 500 and 1200
+select 
+p.name as product,
+count(s.quantity) as totalQuantity,
+sum(s.priceperunit * s.quantity) as totalValue
+from products as p
+inner join sales as s on p.productID = s.productID
+group by s.productID
+having p.name = "Eagles: Hotel California";
+
+
+/* joins: find Product name, reviewer name, rating, and comment on the Visio TV. (only return for the lowest rating!) */
+select * from reviews as r
+inner join products as p
+where p.name = "Visio TV" and r.Rating = 1
+order by rating;
+
+-- ------------------------------------------ Extra - May be difficult
+
+/* Your goal is to write a query that serves as an employee sales report.
+
+This query should return the employeeID, the employee's first and last name, the name of each product, how many of that product they sold */
+select employeeID, FirstName, Lastname from employees
+
